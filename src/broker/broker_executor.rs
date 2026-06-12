55a8acde53d3c74bec479d1e3b4afff81e1387e4
greatchinex <greatchinex@gmail.com>
2026-06-12 19:41:53 +0100

    change dequeue implementation to move to processing hashmap

--- src/broker/broker_executor.rs
+++ src/broker/broker_executor.rs
@@ -1,29 +1,41 @@
-use std::collections::VecDeque;
+use std::collections::{HashMap, VecDeque};
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Job {
+    pub id: String,
     pub payload: String,
 }
 
 #[derive(Debug)]
 pub struct BrokerState {
     pub queued: VecDeque<Job>,
+    pub processing: HashMap<String, Job>,
+    pub next_id: u64,
 }
 
 impl BrokerState {
     pub fn new() -> BrokerState {
         BrokerState {
             queued: VecDeque::new(),
+            processing: HashMap::new(),
+            next_id: 1,
         }
     }
 
     pub fn enqueue(&mut self, payload: String) -> &Self {
-        self.queued.push_back(Job { payload });
+        self.queued.push_back(Job {
+            id: format!("job-{}", self.next_id),
+            payload,
+        });
+
+        self.next_id = self.next_id + 1;
         self
     }
 
     pub fn dequeue(&mut self) -> Option<Job> {
-        self.queued.pop_front()
+        self.queued.pop_front().inspect(|job| {
+            self.processing.insert(job.id.to_string(), job.clone());
+        })
     }
 
     pub fn list(&self) {
